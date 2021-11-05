70bf065ce6fd96fbf1ddba8a10cb4f1c87da78c1
Federico López Gómez <fico89@gmail.com>
2021-11-05 13:07:24 +0000

    Adding pagination to genes/search endpoint

--- src/main/java/org/mousephenotype/api/repositories/GeneRepository.java
+++ src/main/java/org/mousephenotype/api/repositories/GeneRepository.java
@@ -3,10 +3,14 @@ package org.mousephenotype.api.repositories;
 import org.bson.types.ObjectId;
 import org.mousephenotype.api.models.Gene;
 import org.mousephenotype.api.models.GeneBundle;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public interface GeneRepository extends MongoRepository<Gene, String> {
-    ArrayList<Gene> findAllBySignificantMpTermIdsContains(String mpTermId);
+public interface GeneRepository extends PagingAndSortingRepository<Gene, String> {
+    Page<Gene> findAllBySignificantMpTermIdsContains(List<String> mpTermIds, Pageable pageable);
 }
